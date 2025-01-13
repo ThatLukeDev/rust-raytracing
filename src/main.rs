72938ce40bab3feb5ff3c80ff6ee071c6a0ae01e
ThatLukeDev <luke@l2j.co.uk>
2025-01-13 15:10:01 +0000

    vector math

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#[allow(dead_code)]
+
+mod vector;
+
+use vector::Vec3;
+
 fn main() {
-    println!("Hello, world!");
+    let vec_a = Vec3::new(1.0, 2.0, 3.0);
+    let vec_b = Vec3::new(2.0, 4.0, 6.0);
+    let scalar_c = 4.0;
+
+    println!("A: {}", vec_a);
+    println!("B: {}", vec_b);
+    println!("C: {}", scalar_c);
+
+    println!();
+
+    println!("A+B: {}", vec_a + vec_b);
+    println!("A-B: {}", vec_a - vec_b);
+    println!("A*B: {}", vec_a * vec_b);
+    println!("A*C: {}", vec_a * scalar_c);
+    println!("A/C: {}", vec_a / scalar_c);
 }
---
+++ src/vector.rs
@@ -0,0 +1,80 @@
+use std::fmt;
+
+use std::ops::*;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Vec3<T> {
+    pub x: T,
+    pub y: T,
+    pub z: T
+}
+
+impl<T> Vec3<T> {
+    pub fn new(x: T, y: T, z: T) -> Self {
+        Vec3::<T> { x, y, z }
+    }
+}
+
+impl<T: fmt::Display> fmt::Display for Vec3<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({},{},{})", self.x, self.y, self.z)
+    }
+}
+
+impl<T: Add<Output = T>> Add for Vec3<T> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Vec3::<T> {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z
+        }
+    }
+}
+
+impl<T: Sub<Output = T>> Sub for Vec3<T> {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Vec3::<T> {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z
+        }
+    }
+}
+
+impl<T: Copy + Mul<Output = T>> Mul<T> for Vec3<T> {
+    type Output = Self;
+
+    fn mul(self, other: T) -> Self {
+        Vec3::<T> {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other
+        }
+    }
+}
+
+impl<T: Copy + Div<Output = T>> Div<T> for Vec3<T> {
+    type Output = Self;
+
+    fn div(self, other: T) -> Self {
+        Vec3::<T> {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other
+        }
+    }
+}
+
+impl<T: Mul<Output = T> + Add<Output = T>> Mul for Vec3<T> {
+    type Output = T;
+
+    fn mul(self, other: Self) -> T {
+        self.x * other.x +
+        self.y * other.y +
+        self.z * other.z
+    }
+}
