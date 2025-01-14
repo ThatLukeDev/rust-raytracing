932183e45c8b259f56638770fc92691c58344802
ThatLukeDev <luke@l2j.co.uk>
2025-01-14 15:40:56 +0000

    ray init

--- src/ray.rs
+++ src/ray.rs
@@ -9,3 +9,10 @@ pub struct Ray<T> {
     pub origin: Vec3<T>,
     pub direction: Vec3<T>
 }
+
+impl<T: Copy + Mul<Output = T> + Add<Output = T> + From<f64> + Into<f64>> Ray<T>
+    where Vec3<T>: Div<T, Output = Vec3<T>> {
+    pub fn new(origin: Vec3<T>, direction: Vec3<T>) -> Self {
+        Ray::<T> { origin: origin, direction: direction.unit() }
+    }
+}
