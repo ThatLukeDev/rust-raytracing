03ff3689f3289657ddf2404d576cfb5ce2486e9d
ThatLukeDev <luke@l2j.co.uk>
2025-01-15 07:30:33 +0000

    fixed raytrace trait

--- src/raytrace.rs
+++ src/raytrace.rs
@@ -1,6 +1,12 @@
 use crate::vector::Vec3;
 use crate::ray::Ray;
 
-pub trait Raytrace<T: Into<f64>> {
-    fn intersectsAt(ray: &Ray<T>) -> Option<Vec3<T>>;
+use std::ops::*;
+
+pub trait Raytrace<T: Copy + Add<Output = T> + Mul<Output = T> + Div<Output = T> + Sub<Output = T>> {
+    fn intersectsAlong(&self, ray: &Ray<T>) -> Option<T>;
+
+    fn intersectsAt(&self, ray: &Ray<T>) -> Option<Vec3<T>> {
+        Some(ray.at(self.intersectsAlong(ray)?))
+    }
 }
