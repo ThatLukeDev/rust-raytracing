333451a2ec3c2a8114c917aaf9b072b7513d01fa
ThatLukeDev <luke@l2j.co.uk>
2025-01-15 07:37:09 +0000

    raytrace header

--- src/raytrace.rs
+++ src/raytrace.rs
@@ -4,9 +4,9 @@ use crate::ray::Ray;
 use std::ops::*;
 
 pub trait Raytrace<T: Copy + Add<Output = T> + Mul<Output = T> + Div<Output = T> + Sub<Output = T>> {
-    fn intersectsAlong(&self, ray: &Ray<T>) -> Option<T>;
+    fn intersects_along(&self, ray: &Ray<T>) -> Option<T>;
 
-    fn intersectsAt(&self, ray: &Ray<T>) -> Option<Vec3<T>> {
-        Some(ray.at(self.intersectsAlong(ray)?))
+    fn intersects_at(&self, ray: &Ray<T>) -> Option<Vec3<T>> {
+        Some(ray.at(self.intersects_along(ray)?))
     }
 }
--- src/sphere.rs
+++ src/sphere.rs
@@ -1,6 +1,10 @@
 use crate::vector::Vec3;
 use crate::ray::Ray;
+use crate::raytrace::Raytrace;
 
+use std::ops::*;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Sphere<T> {
     origin: Vec3<T>,
     radius: T
@@ -9,5 +13,11 @@ pub struct Sphere<T> {
 impl<T> Sphere<T> {
     pub fn new(origin: Vec3<T>, radius: T) -> Self {
         Sphere::<T> { origin, radius }
+    }
+}
+
+impl<T: Copy + Add<Output = T> + Mul<Output = T> + Div<Output = T> + Sub<Output = T>> Raytrace<T> for Sphere<T> {
+    fn intersects_along(&self, ray: &Ray<T>) -> Option<T> {
+        todo!()
     }
 }
