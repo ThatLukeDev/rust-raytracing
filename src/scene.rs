8c3b4aa4d8c6a3fbaabd89436b17c6c8c8f687b3
ThatLukeDev <luke@l2j.co.uk>
2025-02-03 15:13:48 +0000

    scene raytracing

--- src/scene.rs
+++ src/scene.rs
@@ -1,4 +1,5 @@
 use crate::raytrace::*;
+use crate::ray::*;
 
 use std::ops::*;
 
@@ -11,5 +12,30 @@ struct Scene<T> {
     objects: Vec<Box<dyn Raytrace<T>>>
 }
 
-impl<T: Copy + Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T>> Scene<T> {
+impl<T: Copy + From<f64> + PartialOrd + Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T>> Scene<T> {
+    /// Raytracing.
+    ///
+    /// Traces a ray through the scene, deciding on whether it intersects,
+    /// and what it intersects with.
+    ///
+    /// Returns a tuple of the intersecting object and distance along ray.
+    fn trace(&self, ray: Ray<T>) -> (Option<&Box<dyn Raytrace<T>>>, T) {
+        let mut lowest: T = 9999999999.0.into();
+        let mut closest_obj: Option<&Box<dyn Raytrace<T>>> = None;
+
+        for obj in &self.objects {
+            let distance = obj.intersects_along(&ray);
+            match distance {
+                Some(x) => {
+                    if x < lowest {
+                        lowest = x;
+                        closest_obj = Some(obj);
+                    }
+                },
+                None => ()
+            }
+        }
+
+        (closest_obj, lowest)
+    }
 }
