fa8978051669e2550177c473f00efb127c45cd11
ThatLukeDev <luke@l2j.co.uk>
2025-02-12 07:32:09 +0000

    working multithreading

--- src/scene.rs
+++ src/scene.rs
@@ -1,3 +1,6 @@
+use std::thread;
+use std::sync::mpsc;
+
 use crate::raytrace::*;
 use crate::vector::*;
 use crate::ray::*;
@@ -14,7 +17,7 @@ pub struct Scene<T> {
     /// Every object must implement the Raytrace trait.
     ///
     /// T represents precision of float used throughout the program.
-    pub objects: Vec<Box<dyn Raytrace<T>>>,
+    pub objects: Vec<Box<dyn Raytrace<T> + Sync>>,
 
     /// The camera of the scene.
     pub camera: Camera<T>,
@@ -30,9 +33,9 @@ impl<T: Copy + From<f64> + From<i32> + Into<f64> + PartialOrd + Add<Output = T> 
     /// and what it intersects with.
     ///
     /// Returns a tuple of the intersecting object and distance along ray.
-    pub fn trace(&self, ray: Ray<T>) -> (Option<&Box<dyn Raytrace<T>>>, T) {
+    pub fn trace(&self, ray: Ray<T>) -> (Option<&Box<dyn Raytrace<T> + Sync>>, T) {
         let mut lowest: T = 9999999999.0.into();
-        let mut closest_obj: Option<&Box<dyn Raytrace<T>>> = None;
+        let mut closest_obj = None;
 
         for obj in &self.objects {
             let distance = obj.intersects_along(&ray);
@@ -65,35 +68,59 @@ impl<T: Copy + From<f64> + From<i32> + Into<f64> + PartialOrd + Add<Output = T> 
     }
 
     /// Runs the trace_bounce function multiple times for each pixel.
-    pub fn raytrace<const WIDTH: usize, const HEIGHT: usize>(&self, rays: usize, depth: usize, fov: f64) -> Image<WIDTH, HEIGHT> {
+    pub fn raytrace<const WIDTH: usize, const HEIGHT: usize>(&self, rays: usize, depth: usize, fov: f64) -> Image<WIDTH, HEIGHT>
+    where T: Sync {
         let mut img: Image<WIDTH, HEIGHT> = Image::new();
 
         let aspect_ratio = img.data.len() as f64 / img.data[0].len() as f64;
         let fov_distance = (fov / 2.0).to_radians().tan();
 
-        for x in 0..img.data.len() {
-            for y in 0..img.data[x].len() {
-                let abs_x = 1.0 - (x as f64 / img.data.len() as f64) * 2.0;
-                let abs_y = 1.0 - (y as f64 / img.data[x].len() as f64) * 2.0;
+        let (tx, rx) = mpsc::channel();
+        thread::scope(|s| {
+            for x in 0..WIDTH {
+                let tx_thread = tx.clone();
 
-                let mut color = Color::new(0.0, 0.0, 0.0);
-                let mut bounces = 0.0 as f64;
+                s.spawn(move || {
+                    let mut batch = [Color::new(0.0, 0.0, 0.0); HEIGHT];
 
-                for _i in 0..rays {
-                    let camera_ray = Ray::new(
-                        self.camera.position,
-                        self.camera.transform(Vec3::new((abs_x * aspect_ratio * fov_distance).into(), (abs_y * fov_distance).into(), (1.0).into()).unit())
-                    );
+                    for y in 0..HEIGHT {
+                        let abs_x = 1.0 - (x as f64 / WIDTH as f64) * 2.0;
+                        let abs_y = 1.0 - (y as f64 / HEIGHT as f64) * 2.0;
 
-                    color = color + self.trace_bounce(camera_ray, depth) * 0.001;
-                    bounces += 1.0;
-                }
+                        let mut color = Color::new(0.0, 0.0, 0.0);
+                        let mut bounces = 0.0 as f64;
 
-                color = color / bounces;
+                        for _i in 0..rays {
+                            let camera_ray = Ray::new(
+                                self.camera.position,
+                                self.camera.transform(Vec3::new((abs_x * aspect_ratio * fov_distance).into(), (abs_y * fov_distance).into(), (1.0).into()).unit())
+                            );
 
-                img[x][y] = color;
+                            color = color + self.trace_bounce(camera_ray, depth) * 0.001;
+                            bounces += 1.0;
+                        }
+
+                        color = color / bounces;
+
+                        batch[y] = color;
+                    }
+
+                    tx_thread.send((x, batch)).unwrap();
+                });
             }
-        }
+
+            let mut counter = 0;
+
+            for x in rx {
+                img[x.0] = x.1;
+
+                counter += 1;
+
+                if counter >= WIDTH {
+                    break;
+                }
+            }
+        });
 
         img
     }
