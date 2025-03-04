7471d8c394f6587ecad7b855f396bfc7f507f012
ThatLukeDev <luke@l2j.co.uk>
2025-03-04 15:50:02 +0000

    transmission

--- src/plane.rs
+++ src/plane.rs
@@ -17,13 +17,16 @@ pub struct Plane<T> {
     /// Normal vector of the plane.
     pub normal: Vec3<T>,
 
-    /// The offset of the plane
+    /// The offset of the plane.
     ///
     /// k in `x+y+z = k`
     pub offset: T,
 
     /// Colour of the plane.
     pub color: Color,
+
+    /// The uniformity of transmission.
+    pub roughness: f64,
 }
 
 impl<T: PartialOrd + From<f64> + Into<f64> + Copy + Add<Output = T> + Mul<Output = T> + Div<Output = T> + Sub<Output = T>> Raytrace<T> for Plane<T> {
@@ -32,7 +35,16 @@ impl<T: PartialOrd + From<f64> + Into<f64> + Copy + Add<Output = T> + Mul<Output
     }
 
     fn transmit(&self, ray: &Ray<T>) -> Option<Ray<T>> {
-        todo!();
+        // Uses ThreadRng::Default() so is not re-seeded.
+        let mut rng = rand::thread_rng();
+
+        let pos: Vec3<T> = self.intersects_at(ray)?;
+
+        let direction = ray.direction - (self.normal * (self.normal * ray.direction) * T::from(2.0));
+
+        let random: Vec3<T> = Vec3::new(rng.gen_range(-1.0..1.0).into(), rng.gen_range(-1.0..1.0).into(), rng.gen_range(-1.0..1.0).into());
+
+        Some(Ray::new(pos, direction + random * <f64 as Into<T>>::into(self.roughness)))
     }
 
     fn recolor(&self, _ray: &Ray<T>, color: Color) -> Color {
